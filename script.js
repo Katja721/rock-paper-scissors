83d97dccf2aba12615635d87f6b29a47c5021dee
Katja721 <katja.debevc72@gmail.com>
2022-04-02 12:21:52 +0200

    responsive design

--- script.js
+++ script.js
@@ -1,80 +1,80 @@
-"use strict";
-let media = window.matchMedia("(max-width: 399px)"); //phones to 399px
-let media2 = window.matchMedia("(min-width:400px)" && "(max-width:699px)"); //phones above 400px
-let media3 = window.matchMedia("(min-width:700px)" && "(max-width:1024px)"); //tablets
+'use strict';
+
+let media = window.matchMedia('(max-width: 399px)'); //phones to 399px
+let media2 = window.matchMedia('(min-width:400px)' && '(max-width:699px)'); //phones above 400px
+let media3 = window.matchMedia('(min-width:700px)' && '(max-width:1024px)'); //tablets
 console.log(media, media2, media3);
 //player buttons
-const rockBtn = document.querySelector(".btnRock");
-const rock = document.querySelector(".rock");
-const paperBtn = document.querySelector(".btnPaper");
-const paper = document.querySelector(".paper");
-const scissorsBtn = document.querySelector(".btnScissors");
-const scissors = document.querySelector(".scissors");
+const rockBtn = document.querySelector('.btnRock');
+const rock = document.querySelector('.rock');
+const paperBtn = document.querySelector('.btnPaper');
+const paper = document.querySelector('.paper');
+const scissorsBtn = document.querySelector('.btnScissors');
+const scissors = document.querySelector('.scissors');
 
 //house buttons
-const btnRockHouse = document.querySelector(".btnRockHouse");
-const btnPaperHouse = document.querySelector(".btnPaperHouse");
-const btnScissorsHouse = document.querySelector(".btnScissorsHouse");
+const btnRockHouse = document.querySelector('.btnRockHouse');
+const btnPaperHouse = document.querySelector('.btnPaperHouse');
+const btnScissorsHouse = document.querySelector('.btnScissorsHouse');
 
 //other elements
-const close = document.querySelector(".close");
-const moduleElement = document.querySelector(".module");
-const rules = document.querySelector(".rules");
-const overlay = document.querySelector(".overlay");
-const triangle = document.querySelector(".triangle");
-const scoreElement = document.querySelector(".score");
-const again = document.querySelector(".again");
-const attribution = document.querySelector(".attribution");
+const close = document.querySelector('.close');
+const moduleElement = document.querySelector('.module');
+const rules = document.querySelector('.rules');
+const overlay = document.querySelector('.overlay');
+const triangle = document.querySelector('.triangle');
+const scoreElement = document.querySelector('.score');
+const again = document.querySelector('.again');
+const attribution = document.querySelector('.attribution');
 let score = 0;
-let resultParagraph = document.querySelector(".resultParagraph");
-let computerChoices = ["rock", "paper", "scissors"];
+let resultParagraph = document.querySelector('.resultParagraph');
+let computerChoices = ['rock', 'paper', 'scissors'];
 let computerChoose = computerChoices[Math.floor(Math.random() * 3)];
-console.log(computerChoose);
 let playerChoose;
 const win = function (element) {
   element.style.boxShadow =
-    "0px 0px 0px 10px hsla(212, 14%, 21%, 0.5),0px 0px 0px 30px hsla(225, 33%, 24%, 0.5), 0px 0px 0px 50px hsla(224, 37%, 24%, 0.5)";
-  resultParagraph.textContent = "YOU  WIN";
+    '0px 0px 0px 10px hsla(212, 14%, 21%, 0.5),0px 0px 0px 30px hsla(225, 33%, 24%, 0.5), 0px 0px 0px 50px hsla(224, 37%, 24%, 0.5)';
+  resultParagraph.textContent = 'YOU  WIN';
   score++;
 };
 const lose = function (element) {
-  resultParagraph.textContent = "YOU LOSE";
+  resultParagraph.textContent = 'YOU LOSE';
   score--;
   element.style.boxShadow =
-    "0px 0px 0px 10px hsla(212, 14%, 21%, 0.5),0px 0px 0px 30px hsla(225, 33%, 24%, 0.5), 0px 0px 0px 50px hsla(224, 37%, 24%, 0.5)";
+    '0px 0px 0px 10px hsla(212, 14%, 21%, 0.5),0px 0px 0px 30px hsla(225, 33%, 24%, 0.5), 0px 0px 0px 50px hsla(224, 37%, 24%, 0.5)';
 };
 const draw = function () {
-  resultParagraph.textContent = "DRAW";
+  resultParagraph.textContent = 'DRAW';
 };
 const toggleElements = function () {
-  triangle.classList.add("hidden");
-  again.classList.remove("hidden");
+  triangle.classList.add('hidden');
+  again.classList.remove('hidden');
   if (media.matches) {
-    attribution.style.marginTop = "17rem";
+    attribution.style.marginTop = '17rem';
   } else {
-    attribution.style.marginTop = "11rem";
+    attribution.style.marginTop = '11rem';
   }
-  resultParagraph.classList.remove("hidden");
+  resultParagraph.classList.remove('hidden');
 };
 const newScore = function () {
   scoreElement.textContent = score;
 };
 const house = function (element) {
   if (media.matches) {
-    element.style.left = "14rem";
-    element.style.transform = "scale(1)";
+    element.style.left = '14rem';
+    element.style.transform = 'scale(1)';
   } else if (media2.matches) {
-    element.style.transform = "scale(1)";
-    element.style.left = "18rem";
+    element.style.transform = 'scale(1)';
+    element.style.left = '18rem';
   } else if (media3.matches) {
-    element.style.transform = "scale(1)";
-    element.style.left = "39rem";
+    element.style.transform = 'scale(1.5)';
+    element.style.left = '39rem';
   } else {
-    element.style.left = "62rem";
-    element.style.transform = "scale(2)";
+    element.style.left = '72rem';
+    element.style.transform = 'scale(1.5)';
   }
-  element.style.top = "-0.5rem";
-  element.classList.remove("hidden");
+  element.style.top = '-0.5rem';
+  element.classList.remove('hidden');
   // if (media.matches) {
   //   element.style.transform = "scale(1)";
   // } else {
@@ -82,18 +82,21 @@ const house = function (element) {
   // }
 };
 const player = function (elem) {
-  if (media.matches || media2.matches || media3.matches) {
-    elem.style.left = "1rem";
-    elem.style.transform = "scale(1)";
+  if (media.matches || media2.matches) {
+    elem.style.left = '1rem';
+    elem.style.transform = 'scale(1)';
     // } else if (media2.matches) {
     //   elem.style.left = "1rem";
     //   elem.style.transform = "scale(1)";
     //
+  } else if (media3.matches) {
+    elem.style.left = '1rem';
+    elem.style.transform = 'scale(1.5)';
   } else {
-    elem.style.left = "10rem";
-    elem.style.transform = "scale(2)";
+    elem.style.left = '16rem';
+    elem.style.transform = 'scale(1.5)';
   }
-  elem.style.top = "7rem";
+  elem.style.top = '7rem';
   // if (media.matches) {
   //   elem.style.transform = "scale(1)";
   // } else {
@@ -101,28 +104,37 @@ const player = function (elem) {
   // }
 };
 const removeElement = function (element) {
-  element.classList.add("hidden");
+  element.classList.add('hidden');
 };
+console.log(media.matches);
+console.log(media2.matches);
+console.log(media3.matches);
 
 const shadowAgain = function (item) {
-  item.style.boxShadow = "initial";
-  item.style.transform = "scale(1)";
+  item.style.boxShadow = 'initial';
+  if (media.matches || media2.matches) {
+    item.style.transform = 'scale(1)';
+  } else if (media3.matches) {
+    item.style.transform = 'scale(1.5)';
+  } else {
+    item.style.transform = 'scale(1.5)';
+  }
 };
 const againMoveElem = function (elem, valueLeft, valueTop) {
   elem.style.left = `${valueLeft}`;
   elem.style.top = `${valueTop}`;
 };
-rock.addEventListener("click", function () {
+rock.addEventListener('click', function () {
   playerChoose = rock.alt;
   removeElement(scissorsBtn);
   removeElement(paperBtn);
   toggleElements();
   player(rockBtn);
   console.log(playerChoose);
-  if (computerChoose === "scissors") {
+  if (computerChoose === 'scissors') {
     house(btnScissorsHouse);
     win(rockBtn);
-  } else if (computerChoose === "paper") {
+  } else if (computerChoose === 'paper') {
     house(btnPaperHouse);
     lose(btnPaperHouse);
   } else {
@@ -131,17 +143,17 @@ rock.addEventListener("click", function () {
   }
   newScore();
 });
-paper.addEventListener("click", function () {
+paper.addEventListener('click', function () {
   removeElement(rockBtn);
   removeElement(scissorsBtn);
 
   toggleElements();
   player(paperBtn);
   playerChoose = paper.alt;
-  if (computerChoose === "rock") {
+  if (computerChoose === 'rock') {
     house(btnRockHouse);
     win(paperBtn);
-  } else if (computerChoose === "scissors") {
+  } else if (computerChoose === 'scissors') {
     house(btnScissorsHouse);
     lose(btnScissorsHouse);
   } else {
@@ -150,16 +162,16 @@ paper.addEventListener("click", function () {
   }
   newScore();
 });
-scissors.addEventListener("click", function () {
+scissors.addEventListener('click', function () {
   playerChoose = scissors.alt;
   removeElement(paperBtn);
   removeElement(rockBtn);
   toggleElements();
   player(scissorsBtn);
-  if (computerChoose === "paper") {
+  if (computerChoose === 'paper') {
     house(btnPaperHouse);
     win(scissorsBtn);
-  } else if (computerChoose === "rock") {
+  } else if (computerChoose === 'rock') {
     house(btnRockHouse);
     lose(btnRockHouse);
   } else {
@@ -168,53 +180,53 @@ scissors.addEventListener("click", function () {
   }
   newScore();
 });
-close.addEventListener("click", function () {
-  overlay.classList.add("hidden");
+close.addEventListener('click', function () {
+  overlay.classList.add('hidden');
 });
-rules.addEventListener("click", function () {
-  overlay.classList.remove("hidden");
+rules.addEventListener('click', function () {
+  overlay.classList.remove('hidden');
 });
 
-again.addEventListener("click", function () {
+again.addEventListener('click', function () {
   computerChoose = computerChoices[Math.floor(Math.random() * 3)];
   // removeElement(rockBtn);
   // removeElement(paperBtn);
   // removeElement(paperBtn);
   removeElement(again);
-  rockBtn.classList.remove("hidden");
-  paperBtn.classList.remove("hidden");
-  scissorsBtn.classList.remove("hidden");
-  triangle.classList.remove("hidden");
+  rockBtn.classList.remove('hidden');
+  paperBtn.classList.remove('hidden');
+  scissorsBtn.classList.remove('hidden');
+  triangle.classList.remove('hidden');
   if (media2.matches || media3.matches) {
-    attribution.style.marginTop = "9rem";
+    attribution.style.marginTop = '9rem';
   } else {
-    attribution.style.marginTop = "0rem";
+    attribution.style.marginTop = '0rem';
   }
-  btnRockHouse.classList.add("hidden");
-  btnPaperHouse.classList.add("hidden");
-  btnScissorsHouse.classList.add("hidden");
-  resultParagraph.classList.add("hidden");
+  btnRockHouse.classList.add('hidden');
+  btnPaperHouse.classList.add('hidden');
+  btnScissorsHouse.classList.add('hidden');
+  resultParagraph.classList.add('hidden');
   // removeElement(btnPaperHouse);
   // removeElement(btnRockHouse);
   // removeElement(btnScissorsHouse);
   // removeElement(triangle);
   //first number left, second value top
   if (media.matches) {
-    againMoveElem(rockBtn, "8rem", "20rem");
-    againMoveElem(paperBtn, "-7rem", "4rem");
-    againMoveElem(scissorsBtn, "0.5rem", "4rem");
+    againMoveElem(rockBtn, '8rem', '20rem');
+    againMoveElem(paperBtn, '-7rem', '4rem');
+    againMoveElem(scissorsBtn, '0.5rem', '4rem');
   } else if (media2.matches) {
-    againMoveElem(rockBtn, "10rem", "20rem");
-    againMoveElem(paperBtn, "-6rem", "4rem");
-    againMoveElem(scissorsBtn, "3.5rem", "4rem");
+    againMoveElem(rockBtn, '10rem', '20rem');
+    againMoveElem(paperBtn, '-6rem', '4rem');
+    againMoveElem(scissorsBtn, '3.5rem', '4rem');
   } else if (media3.matches) {
-    againMoveElem(rockBtn, "20rem", "20rem");
-    againMoveElem(paperBtn, "4rem", "3rem");
-    againMoveElem(scissorsBtn, "14.5rem", "3rem");
+    againMoveElem(rockBtn, '20rem', '20rem');
+    againMoveElem(paperBtn, '4rem', '3rem');
+    againMoveElem(scissorsBtn, '14.5rem', '3rem');
   } else {
-    againMoveElem(rockBtn, "38rem", "-8rem");
-    againMoveElem(paperBtn, "23rem", "-23rem");
-    againMoveElem(scissorsBtn, "32rem", "-23rem");
+    againMoveElem(rockBtn, '46rem', '-8rem');
+    againMoveElem(paperBtn, '30rem', '-23rem');
+    againMoveElem(scissorsBtn, '40rem', '-23rem');
   }
   shadowAgain(btnScissorsHouse);
   shadowAgain(btnRockHouse);
---
+++ scriptVersion2.js
@@ -0,0 +1,321 @@
+'use strict';
+
+//media queries
+let media = window.matchMedia('(max-width: 399px)'); //phones to 399px
+let media2 =
+  window.matchMedia('(min-width:400px)') &&
+  window.matchMedia('(max-width: 699px)'); //phones above 400px
+let media3 = window.matchMedia('(min-width:700px)'); //tablets
+console.log(media);
+console.log(media2);
+console.log(media3);
+//player buttons
+const paperBtn2 = document.querySelector('.paperBtn2');
+const scissorsBtn2 = document.querySelector('.scissorsBtn2');
+const rockBtn2 = document.querySelector('.rockBtn2');
+const lizardBtn2 = document.querySelector('.lizardBtn2');
+const spockBtn2 = document.querySelector('.spockBtn2');
+
+//house buttons
+const paperBtnHouse2 = document.querySelector('.paperBtnHouse2');
+const scissorsBtnHouse2 = document.querySelector('.scissorsBtnHouse2');
+const rockBtnHouse2 = document.querySelector('.rockBtnHouse2');
+const lizardBtnHouse2 = document.querySelector('.lizardBtnHouse2');
+const spockBtnHouse2 = document.querySelector('.spockBtnHouse2');
+
+//other buttons
+const again = document.querySelector('.again2');
+const rules = document.querySelector('.rules2');
+const close = document.querySelector('.close');
+
+//other elements
+const resultParagraph = document.querySelector('.resultParagraph2');
+let scoreElement = document.querySelector('.score');
+const overlay = document.querySelector('.overlay');
+const pentagon = document.querySelector('.pentagon');
+//choices
+const computerChoices = ['paper', 'scissors', 'rock', 'lizard', 'spock'];
+let computerChoose = computerChoices[Math.floor(Math.random() * 5)];
+let playerChoose;
+
+console.log(computerChoose);
+let score = 0;
+
+//functions
+console.log(media.matches);
+let displayHouseBtn = document.querySelector(`.${computerChoose}BtnHouse2`);
+const removeUnselected = function (selected, ...others) {
+  selected.classList.remove('hidden');
+  if (media.matches || media2.matches) {
+    selected.style.transform = 'scale(1)';
+  } else if (media3.matches) {
+    selected.style.transform = 'scale(1.5)';
+  } else {
+    selected.style.transform = 'scale(2)';
+  }
+  for (let items of others) {
+    items.classList.add('hidden');
+  }
+};
+const showPlayerChoices = function () {
+  for (let options = 0; options < computerChoices.length; options++) {
+    //show all player options
+    document
+      .querySelector(`.${computerChoices[options]}Btn2`)
+      .classList.remove('hidden');
+    //remove house choose
+    document
+      .querySelector(`.${computerChoices[options]}BtnHouse2`)
+      .classList.add('hidden');
+
+    document.querySelector(`.${computerChoices[options]}Btn2`).style.transform =
+      'scale(1)';
+    document.querySelector(`.${computerChoices[options]}Btn2`).style.boxShadow =
+      'initial';
+  }
+};
+const moveElement = function (element, leftValue, topValue) {
+  element.style.left = `${leftValue}`;
+  element.style.top = `${topValue}`;
+};
+//click on buttons
+//rock against paper||lizard win  rock draw else lose
+rockBtn2.addEventListener('click', function () {
+  displayHouseBtn.classList.remove('hidden');
+  again.classList.remove('hidden');
+  resultParagraph.classList.remove('hidden');
+
+  removeUnselected(
+    rockBtn2,
+    scissorsBtn2,
+    paperBtn2,
+    lizardBtn2,
+    spockBtn2,
+    pentagon
+  );
+  moveElement(rockBtn2, '1rem', '8rem');
+
+  if (computerChoose === 'paper' || computerChoose === 'lizard') {
+    resultParagraph.textContent = 'YOU WIN';
+    score++;
+    rockBtn2.style.boxShadow =
+      '0px 0px 0px 10px hsla(212, 14%, 21%, 0.5),0px 0px 0px 30px hsla(225, 33%, 24%, 0.5), 0px 0px 0px 50px hsla(224, 37%, 24%, 0.5)';
+  } else if (computerChoose === 'rock') {
+    resultParagraph.textContent = 'DRAW';
+  } else {
+    resultParagraph.textContent = 'YOU LOSE';
+    document.querySelector(`.${computerChoose}BtnHouse2`).style.boxShadow =
+      '0px 0px 0px 10px hsla(212, 14%, 21%, 0.5),0px 0px 0px 30px hsla(225, 33%, 24%, 0.5), 0px 0px 0px 50px hsla(224, 37%, 24%, 0.5)';
+    score--;
+  }
+  scoreElement.textContent = score;
+});
+//paper against scissors || spock win paper draw else lose
+paperBtn2.addEventListener('click', function () {
+  displayHouseBtn.classList.remove('hidden');
+  again.classList.remove('hidden');
+  resultParagraph.classList.remove('hidden');
+
+  removeUnselected(
+    paperBtn2,
+    scissorsBtn2,
+    rockBtn2,
+    lizardBtn2,
+    spockBtn2,
+    pentagon
+  );
+  moveElement(paperBtn2, '1rem', '8rem');
+
+  if (computerChoose === 'scissors' || computerChoose === 'spock') {
+    resultParagraph.textContent = 'YOU WIN';
+    paperBtn2.style.boxShadow =
+      '0px 0px 0px 10px hsla(212, 14%, 21%, 0.5),0px 0px 0px 30px hsla(225, 33%, 24%, 0.5), 0px 0px 0px 50px hsla(224, 37%, 24%, 0.5)';
+    score++;
+  } else if (computerChoose === 'paper') {
+    resultParagraph.textContent = 'DRAW';
+  } else {
+    console.log('you lose');
+    resultParagraph.textContent = 'YOU LOSE';
+    document.querySelector(`.${computerChoose}BtnHouse2`).style.boxShadow =
+      '0px 0px 0px 10px hsla(212, 14%, 21%, 0.5),0px 0px 0px 30px hsla(225, 33%, 24%, 0.5), 0px 0px 0px 50px hsla(224, 37%, 24%, 0.5)';
+    score--;
+  }
+  scoreElement.textContent = score;
+});
+//scissors against paper || lizard win scissors draw else lose
+scissorsBtn2.addEventListener('click', function () {
+  displayHouseBtn.classList.remove('hidden');
+  again.classList.remove('hidden');
+  resultParagraph.classList.remove('hidden');
+
+  removeUnselected(
+    scissorsBtn2,
+    paperBtn2,
+    rockBtn2,
+    lizardBtn2,
+    spockBtn2,
+    pentagon
+  );
+  moveElement(scissorsBtn2, '1rem', '8rem');
+  if (computerChoose === 'paper' || computerChoose === 'lizard') {
+    resultParagraph.textContent = 'YOU WIN';
+    scissorsBtn2.style.boxShadow =
+      '0px 0px 0px 10px hsla(212, 14%, 21%, 0.5),0px 0px 0px 30px hsla(225, 33%, 24%, 0.5), 0px 0px 0px 50px hsla(224, 37%, 24%, 0.5)';
+    score++;
+  } else if (computerChoose === 'scissors') {
+    resultParagraph.textContent = 'DRAW';
+  } else {
+    resultParagraph.textContent = 'YOU LOSE';
+    document.querySelector(`.${computerChoose}BtnHouse2`).style.boxShadow =
+      '0px 0px 0px 10px hsla(212, 14%, 21%, 0.5),0px 0px 0px 30px hsla(225, 33%, 24%, 0.5), 0px 0px 0px 50px hsla(224, 37%, 24%, 0.5)';
+    score--;
+  }
+  scoreElement.textContent = score;
+});
+//lizard against paper|| spock win lizard draw else lose
+lizardBtn2.addEventListener('click', function () {
+  displayHouseBtn.classList.remove('hidden');
+  again.classList.remove('hidden');
+  resultParagraph.classList.remove('hidden');
+
+  removeUnselected(
+    lizardBtn2,
+    scissorsBtn2,
+    rockBtn2,
+    paperBtn2,
+    spockBtn2,
+    pentagon
+  );
+  moveElement(lizardBtn2, '1rem', '8rem');
+  if (computerChoose === 'paper' || computerChoose === 'spock') {
+    resultParagraph.textContent = 'YOU WIN';
+    lizardBtn2.style.boxShadow =
+      '0px 0px 0px 10px hsla(212, 14%, 21%, 0.5),0px 0px 0px 30px hsla(225, 33%, 24%, 0.5), 0px 0px 0px 50px hsla(224, 37%, 24%, 0.5)';
+    score++;
+  } else if (computerChoose === 'lizard') {
+    resultParagraph.textContent = 'DRAW';
+  } else {
+    resultParagraph.textContent = 'YOU LOSE';
+    document.querySelector(`.${computerChoose}BtnHouse2`).style.boxShadow =
+      '0px 0px 0px 10px hsla(212, 14%, 21%, 0.5),0px 0px 0px 30px hsla(225, 33%, 24%, 0.5), 0px 0px 0px 50px hsla(224, 37%, 24%, 0.5)';
+
+    score--;
+  }
+  scoreElement.textContent = score;
+});
+//spock against rock || scissors win lizard draw else lose
+spockBtn2.addEventListener('click', function () {
+  displayHouseBtn.classList.remove('hidden');
+  again.classList.remove('hidden');
+  resultParagraph.classList.remove('hidden');
+  removeUnselected(
+    spockBtn2,
+    scissorsBtn2,
+    rockBtn2,
+    lizardBtn2,
+    paperBtn2,
+    pentagon
+  );
+  moveElement(spockBtn2, '1rem', '8rem');
+  if (computerChoose === 'rock' || computerChoose === 'scissors') {
+    resultParagraph.textContent = 'YOU WIN';
+    spockBtn2.style.boxShadow =
+      '0px 0px 0px 10px hsla(212, 14%, 21%, 0.5),0px 0px 0px 30px hsla(225, 33%, 24%, 0.5), 0px 0px 0px 50px hsla(224, 37%, 24%, 0.5)';
+    score++;
+  } else if (computerChoose === 'spock') {
+    resultParagraph.textContent = 'DRAW';
+  } else {
+    resultParagraph.textContent = 'YOU LOSE';
+    document.querySelector(`.${computerChoose}BtnHouse2`).style.boxShadow =
+      '0px 0px 0px 10px hsla(212, 14%, 21%, 0.5),0px 0px 0px 30px hsla(225, 33%, 24%, 0.5), 0px 0px 0px 50px hsla(224, 37%, 24%, 0.5)';
+    score--;
+  }
+  scoreElement.textContent = score;
+});
+//show rules
+rules.addEventListener('click', function () {
+  overlay.classList.remove('hidden');
+});
+//hide rules
+close.addEventListener('click', function () {
+  overlay.classList.add('hidden');
+});
+overlay.addEventListener('click', function () {
+  overlay.classList.add('hidden');
+});
+//paly again
+again.addEventListener('click', function () {
+  computerChoose = computerChoices[Math.floor(Math.random() * 5)];
+  displayHouseBtn = document.querySelector(`.${computerChoose}BtnHouse2`);
+
+  console.log(computerChoose);
+
+  resultParagraph.classList.add('hidden');
+  again.classList.add('hidden');
+  pentagon.classList.remove('hidden');
+
+  //first number left second number top
+  if (media.matches) {
+    moveElement(rockBtn2, '13rem', '20rem');
+  } else if (media2.matches) {
+    moveElement(rockBtn2, '16rem', '20rem');
+  } else if (media3.matches) {
+    moveElement(rockBtn2, '26rem', '14rem');
+  } else {
+    moveElement(rockBtn2, '51rem', '22rem');
+  }
+  if (media.matches) {
+    moveElement(paperBtn2, '9rem', '9rem');
+  } else if (media2.matches) {
+    moveElement(paperBtn2, '12rem', '10rem');
+  } else if (media3.matches) {
+    moveElement(paperBtn2, '23rem', '3rem');
+  } else {
+    moveElement(paperBtn2, '48rem', '11rem');
+  }
+
+  if (media.matches) {
+    moveElement(scissorsBtn2, '-6rem', '3rem');
+  } else if (media2.matches) {
+    moveElement(scissorsBtn2, '-3rem', '3rem');
+  } else if (media3.matches) {
+    moveElement(scissorsBtn2, '6rem', '-3rem');
+  } else {
+    moveElement(scissorsBtn2, '31rem', '4rem');
+  }
+  if (media.matches) {
+    moveElement(lizardBtn2, '0rem', '13.5rem');
+  } else if (media2.matches) {
+    moveElement(lizardBtn2, '4rem', '14rem');
+  } else if (media3.matches) {
+    moveElement(lizardBtn2, '-8rem', '13.5rem');
+  } else {
+    moveElement(lizardBtn2, '17rem', '22.5rem');
+  }
+
+  if (media.matches) {
+    moveElement(spockBtn2, '-9rem', '2rem');
+  } else if (media2.matches) {
+    moveElement(spockBtn2, '-7rem', '2rem');
+  } else if (media3.matches) {
+    moveElement(spockBtn2, '-18rem', '2rem');
+  } else {
+    moveElement(spockBtn2, '6rem', '11rem');
+  }
+  console.log(`.${computerChoose}BtnHouse2`);
+  spockBtn2.style.boxShadow = 'initial';
+  rockBtn2.style.boxShadow = 'initial';
+  scissorsBtn2.style.boxShadow = 'initial';
+  paperBtn2.style.boxShadow = 'initial';
+  lizardBtn2.style.boxShadow = 'initial';
+  //////
+  spockBtnHouse2.style.boxShadow = 'initial';
+  rockBtnHouse2.style.boxShadow = 'initial';
+  scissorsBtnHouse2.style.boxShadow = 'initial';
+  paperBtnHouse2.style.boxShadow = 'initial';
+  lizardBtnHouse2.style.boxShadow = 'initial';
+
+  document.querySelector(`.${computerChoose}BtnHouse2`).style.boxShadow =
+    'initial';
+  showPlayerChoices();
+});
